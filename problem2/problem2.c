0c18670772b5f087a0df37c1eddddf535f374b42
Yunpeng Wang <yuwan09@vm-hw06.eecs.tufts.edu>
2023-10-06 19:01:27 -0400

    Complete preliminary procedures for problem2

--- problem2/problem2.c
+++ problem2/problem2.c
@@ -1,11 +1,21 @@
-
+#include <stddef.h>
 #include "problem2.h"
 
 
 void lower(char* str)
 {
-
+    if(str == NULL){
+        return;
+    }
   // Fill in this function
+
+
+    while(*str){
+        if('A'<=*str&&*str<='Z'){
+            *str = *str + 32;
+        }
+        str++;
+    }
 
 }
 
--- problem2/test_problem2.c
+++ problem2/test_problem2.c
@@ -1,12 +1,25 @@
-
+#include <stdio.h>
 #include "problem2.h"
 
 
 int main(int argc, char* argv[])
 {
+    char test1[] = "Wang Yunpeng WYP!";
+    char test2[] = "";
+    char *test3 = NULL;
+    char test4[] = "qwe123123";
 
+    lower(test1);
+    lower(test2);
+    lower(test3);
+    lower(test4);
+
+    printf("%s\n\n", test1);
+    printf("%s\n\n", test2);
+    printf("%s\n\n", test3);
+    printf("%s\n\n", test4);
+
+    return 0;
   // Put anything here that you think is appropriate to test your function
-
-  return(0);
 }
 
