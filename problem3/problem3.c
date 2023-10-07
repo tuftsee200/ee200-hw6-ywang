0a4acc143c09ab9f4cfc43e03ee94efaff424a12
Yunpeng Wang <yuwan09@vm-hw06.eecs.tufts.edu>
2023-10-06 21:31:35 -0400

    Use a new method called make and implement print in the main function

--- problem3/problem3.c
+++ problem3/problem3.c
@@ -1,10 +1,39 @@
-
+#include <stdio.h>
 #include "problem3.h"
 
 void find_frequency(const char* str, int len, unsigned int histogram[26])
 {
+    if(!str){
+        printf("your input is NULL, please don't..\n\n");
+        return;
+    }
+    
+    for(int i=0; i<=25; i++){
+        histogram[i] = 0;      //initialize the histogram to 0
+    }
 
-  // Fill in this function
+    for(int i=0; i<len && str[i]!='\0'; i++){
+        char c1 = str[i];     //traverse the string and esure not to go beyond its bounds
+
+        if(c1>='A' && c1<='Z'){
+            c1 = c1-'A'+'a';     //convert the characters to lowercase
+        }
+
+        if('a'<=c1 && c1<='z'){
+            histogram[c1-'a']++;    //update
+        }
+    }
+
+
+                      //print result
+    printf("the result of your String \"%s\" is:\n", str);
+        for(int i = 0; i <= 25; i++){
+            if(histogram[i]>0){
+                printf("{%c's frequency is %u}\n", 'a'+i, histogram[i]);
+            }
+        }
+
+        printf("\n\n");
 
 }
 
--- problem3/test_problem3.c
+++ problem3/test_problem3.c
@@ -1,9 +1,19 @@
-
+#include <stddef.h>
 #include "problem3.h"
 
 
 int main(int argc, char* argv[])
 {
+    char *test1 = NULL;
+    char test2[] = "do you love me???";
+    char test3[] = "Yunpeng Wang at Tufts University";
+    char test4[] = "";
+    unsigned int h[26];
+
+    find_frequency(test1, 0, h);
+    find_frequency(test2, sizeof(test2)-1, h);
+    find_frequency(test3, sizeof(test3), h);
+    find_frequency(test4, sizeof(test4)-1, h);
 
   // Put anything here that you think is appropriate to test your function
 
