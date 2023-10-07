ddacebeb985b2da0f66f19d17412e1879c0fff14
Yunpeng Wang <yuwan09@vm-hw06.eecs.tufts.edu>
2023-10-06 23:58:58 -0400

    11

--- problem4/problem4.c
+++ problem4/problem4.c
@@ -1,11 +1,33 @@
-
+//#include <stddef>
 #include "problem4.h"
+#include <string.h>
 
-
+//find the shortest string of an array, return the pointer
 const char* find_shortest(const char * const * strings, int len)
 {
+    //assume the first string the shortest one
+    const char* shortest = strings[0];
+    int minL = 0;
 
-  // Fill in this function
+    //find the first non-NULL string
+    while(!shortest && minL<len){
+        shortest = strings[minL];
+        minL++;
+    }
+
+    //all Null return Null
+    if(!shortest)
+        return NULL;
+
+    //iterate through the remaining
+    for(int i = minL; i<len; i++){
+        if(strings[i] && strlen(strings[i])<strlen(shortest)){
+            shortest = strings[i];
+        }
+    }
+
+    return shortest;
+
 
 }
 
--- problem4/test_problem4.c
+++ problem4/test_problem4.c
@@ -1,10 +1,20 @@
-
+#include "stdio.h"
 #include "problem4.h"
 
 
 int main(int argc, char* argv[])
 {
+    const char* strings1[] = {"apple", "kiwi", "durian"};
+    const char* strings2[] = {NULL, "apple", "kiwi", "durian"};
+    const char* strings3[] = {"apple", NULL, "kiwi", "durian"};
+    const char* strings4[] = {NULL, NULL};
+    const char* strings5[] = {"durian"};
 
+    printf("shortest string in strings1 is: %s\n", find_shortest(strings1, 3));
+    printf("shortest string in strings2 is: %s\n", find_shortest(strings2, 4));
+    printf("shortest string in strings3 is: %s\n", find_shortest(strings3, 4));
+    printf("shortest string in strings4 is: %s\n", find_shortest(strings4, 2));
+    printf("shortest string in strings5 is: %s\n", find_shortest(strings5, 1));
   // Put anything here that you think is appropriate to test your function
 
   return(0);
