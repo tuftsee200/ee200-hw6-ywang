91caa7d06f0bab0f67c44f01cb7a175f6b12c965
Yunpeng Wang <yuwan09@vm-hw08.eecs.tufts.edu>
2023-10-08 21:07:01 -0400

    do the 1st test

--- problem1/problem1.c
+++ problem1/problem1.c
@@ -3,10 +3,43 @@
 
 char check_win(char board[3][3])
 {
+    //initialize counters;
+    int countX = 0;
+    int countO = 0;
+    int countSpace = 0;
+    //update counter
+    for(int i = 0; i < 3; i++)
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] == 'x') countX++;
+            else if(board[i][j] == 'o') countO++;
+            else if(board[i][j] == ' ') countSpace++;
+            else return '\0';
+        }
 
-  // Fill in this function
-  // You don't need to print anything here; just return the correct value
+    //check various conditions including (space)(cat's game)and(NULL)
 
-  return(0); // Edit this to return something meaningful
+    //check and return if  more 'x' or 'o' than would be possible
+    if(countX - countO > 1 || countO - countX > 1) return '\0';
+    //check and return if the game is still going
+    if(countSpace > 0) return ' ';
+    //won conditions
+    //column and row
+    for(int i = 0; i < 3; i++){
+        if(board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            return board[0][i];
+        if(board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            return board[i][0];
+    }
+    //diagonally
+    if(board[1][1] == board[2][2] && board[2][2] == board[0][0])
+        return board[1][1];
+    if(board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        return board[0][2];
+    //the last condition "no one has won"
+    return '.';
+    
+
+
+  //return(0); // Edit this to return something meaningful
 }
 
--- problem1/test_problem1.c
+++ problem1/test_problem1.c
@@ -1,9 +1,52 @@
-
+#include <stdio.h>
 #include "problem1.h"
 
 
 int main(int argc, char* argv[])
 {
+    char B1[3][3] = {
+        {'x', 'x', 'x'},
+        {'o', 'x', 'o'},
+        {'o', 'o', 'x'}
+    };
+
+    char B2[3][3] = {
+        {'o', 'x', 'x'},
+        {'x', 'o', 'x'},
+        {'o', 'x', 'o'}
+    };
+
+    char B3[3][3] = {
+        {'o', 'o', 'x'},
+        {'o', 'x', 'o'},
+        {'o', 'x', 'x'}
+    };
+
+    char B4[3][3] = {
+        {'$', ' ', '%'},
+        {'o', 'x', 'o'},
+        {'x', 'o', 'x'}
+    };
+
+    char B5[3][3] = {
+        {'x', 'x', 'o'},
+        {'x', 'x', 'o'},
+        {'x', 'x', 'o'}
+    };
+
+    char B6[3][3] = {
+        {'x', 'x', 'o'},
+        {'o', 'o', 'x'},
+        {'x', 'x', 'o'}
+    };
+
+    printf("\nthe result of B1 is %C\n", check_win(B1));
+    printf("\nthe result of B2 is %C\n", check_win(B2));
+    printf("\nthe result of B3 is %C\n", check_win(B3));
+    printf("\nthe result of B4 is %C\n", check_win(B4));
+    printf("\nthe result of B5 is %C\n", check_win(B5));
+    printf("\nthe result of B6 is %C\n", check_win(B6));
+
 
   // Put anything here that you think is appropriate to test your function
 
